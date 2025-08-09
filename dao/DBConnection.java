c38233645c48200953dcaa1301f967a97fd8d4dd
Tirth Makadia <tirthmakadia2006@gmail.com>
2025-08-09 07:35:31 +0530

    change db-name and db-pass DBConnection.java

--- dao/DBConnection.java
+++ dao/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/habit_tracker";
+    private static final String URL = "jdbc:mysql://localhost:3306/your_DB_name";
     private static final String USER = "root";
-    private static final String PASSWORD = "Tirth_22";
+    private static final String PASSWORD = "Your_DB_password";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
